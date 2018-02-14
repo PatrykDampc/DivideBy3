d24fcf73fdc7b5c6dc6d66f7a94d377f9cede283
Patryk Dampc <p.dampc@gmail.com>
2018-02-14 16:24:44 +0100

    3 sec chanigng text

--- app/src/main/java/com/example/patryk/divideby3/MainActivity.java
+++ app/src/main/java/com/example/patryk/divideby3/MainActivity.java
@@ -9,13 +9,14 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.ViewSwitcher;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    ImageButton button;
     TextSwitcher textSwitcher;
 
     @Override
@@ -24,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        button.findViewById(R.id.bumButtonID);
-        textSwitcher.findViewById(R.id.numberTextSwitcherID);
+        button = findViewById(R.id.bumButtonID);
+        textSwitcher = findViewById(R.id.numberTextSwitcherID);
         Animation in = AnimationUtils.loadAnimation(this,
                 android.R.anim.slide_in_left);
         Animation out = AnimationUtils.loadAnimation(this,
@@ -47,7 +48,8 @@ public class MainActivity extends AppCompatActivity {
         final Handler handler =new Handler();
         final Runnable r = new Runnable() {
             public void run() {
-              //  textSwitcher.setText();
+                String text = new RandomNumber().getRanNumString();
+                textSwitcher.setText(text);
                 handler.postDelayed(this, 3000);
             }
         };

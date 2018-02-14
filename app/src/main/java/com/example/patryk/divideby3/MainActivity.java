2e1712dc321b57b6fb70c2b6b06a4aa424b9a437
Patryk Dampc <p.dampc@gmail.com>
2018-02-14 16:10:28 +0100

    mainActivity

--- app/src/main/java/com/example/patryk/divideby3/MainActivity.java
+++ app/src/main/java/com/example/patryk/divideby3/MainActivity.java
@@ -1,21 +1,65 @@
 package com.example.patryk.divideby3;
 
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
+
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
+    Button button;
+    TextSwitcher textSwitcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RandomNumber rn = new RandomNumber();
 
-        textView = (TextView) findViewById(R.id.textView);
-        textView.setText(rn.getRanNumString());
+
+        button.findViewById(R.id.bumButtonID);
+        textSwitcher.findViewById(R.id.numberTextSwitcherID);
+        Animation in = AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_in_left);
+        Animation out = AnimationUtils.loadAnimation(this,
+                android.R.anim.slide_out_right);
+        textSwitcher.setInAnimation(in);
+        textSwitcher.setOutAnimation(out);
+
+        textSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+
+            public View makeView() {
+                TextView myText = new TextView(MainActivity.this);
+                myText.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
+                myText.setTextSize(30);
+                myText.setTextColor(Color.RED);
+                return myText;
+            }
+        });
+
+        final Handler handler =new Handler();
+        final Runnable r = new Runnable() {
+            public void run() {
+              //  textSwitcher.setText();
+                handler.postDelayed(this, 3000);
+            }
+        };
+        handler.postDelayed(r, 0000);
+
+
+
+
 
 
     }
+
+
+
 }

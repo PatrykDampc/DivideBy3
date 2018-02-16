b4563f6df96468b14ef57cbd85fce70fc339ea52
Patryk Dampc <p.dampc@gmail.com>
2018-02-16 10:22:06 +0100

    hogh score

--- app/src/main/java/com/example/patryk/divideby3/MainActivity.java
+++ app/src/main/java/com/example/patryk/divideby3/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.patryk.divideby3;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -16,13 +18,23 @@ import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
 
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String PREFERENCES = "Prefs";
+    public static final String HIGH_SCORE = "HIGH_SCORE_KEY";
+    SharedPreferences prefs;
+    private int highScore;
+
     ImageButton button;
     TextSwitcher textSwitcher;
     TextView scoreView;
+    TextView highScoreView;
+
+
     private RandomNumber rn;
     CountDownTimer cd;
     int i = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +45,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textSwitcher = findViewById(R.id.numberTextSwitcherID);
         scoreView = findViewById(R.id.scoreID);
 
+
         Animation in = AnimationUtils.loadAnimation(this,
                 android.R.anim.slide_in_left);
         Animation out = AnimationUtils.loadAnimation(this,
                 android.R.anim.slide_out_right);
         textSwitcher.setInAnimation(in);
         textSwitcher.setOutAnimation(out);
+
         button.setOnClickListener(this);
-
         textSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
-
             public View makeView() {
                 TextView myText = new TextView(MainActivity.this);
                 myText.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL);
@@ -52,50 +64,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        prefs = getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+        highScore = prefs.getInt(HIGH_SCORE, 0);
+
         cd = new CountDownTimer(3000, 3000) {
             @Override
             public void onTick(long millisUntilFinished) {
-             onTaskCreated();
-            }
-
-            @Override
-            public void onFinish() {
-                if(!rn.isDivisibleByThree()) {
-                   i++;
-                    start();
-                } else {
-                    rn.setDivisibleByThree(false);
-                    Toast.makeText(MainActivity.this, "gówno gówno", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(MainActivity.this, StartActivity.class));
+                rn = new RandomNumber();
+                textSwitcher.setText(rn.getRanNumString());
+                scoreView.setText(String.valueOf(i));
+                if (i++ == highScore) {
+                    Toast.makeText(MainActivity.this, "new record!", Toast.LENGTH_SHORT).show();
                 }
+                if (i > highScore) {
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putInt(HIGH_SCORE, i);
+                    highScoreView.setText(highScore);
+                }
+
+                @Override
+                public void onFinish () {
+                    if (!rn.isDivisibleByThree()) {
+                        i++;
+                        start();
+                    } else {
+                        rn.setDivisibleByThree(false);
+                        Toast.makeText(MainActivity.this, "gówno gówno", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, StartActivity.class));
+                    }
+                }
+            }.start();
+        };
+
+    }
+
+
+        @Override
+        public void onClick (View v){
+            if (rn.isDivisibleByThree()) {
+                Toast.makeText(MainActivity.this, "ok", Toast.LENGTH_SHORT).show();
+                i = i + 2;
+                cd.cancel();
+                cd.start();
+            } else {
+                cd.cancel();
+                Toast.makeText(MainActivity.this, "gówno", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
             }
-        }.start();
-    }
-
-
-    public void onTaskCreated(){
-        rn = new RandomNumber();
-        textSwitcher.setText(rn.getRanNumString());
-        scoreView.setText(String.valueOf(i));
-
-    }
-
-
-
-    @Override
-    public void onClick(View v) {
-        if(rn.isDivisibleByThree()) {
-            Toast.makeText(MainActivity.this, "ok", Toast.LENGTH_SHORT).show();
-            i = i + 2;
-            cd.cancel();
-            cd.start();
-        }else {
-            cd.cancel();
-            Toast.makeText(MainActivity.this, "gówno", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(MainActivity.this, StartActivity.class));
         }
-    }
-
-
-
 }

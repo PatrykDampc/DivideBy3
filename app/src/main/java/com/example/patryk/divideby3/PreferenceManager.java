bd4124702782d197bb9ce17cda8145140c82a95b
Patryk Dampc <p.dampc@gmail.com>
2018-02-18 00:34:21 +0100

    splash screen

---
+++ app/src/main/java/com/example/patryk/divideby3/PreferenceManager.java
@@ -0,0 +1,31 @@
+package com.example.patryk.divideby3;
+
+/**
+ * Created by patryk on 17.02.2018.
+ */
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferenceManager {
+    private static final String IS_FIRST_TIME_LAUNCH = "IS_FIRST_TIME_LAUNCH_KEY";
+
+   private SharedPreferences prefs;
+   private SharedPreferences.Editor editor;
+   private Context context;
+
+    public PreferenceManager(Context context) {
+        this.context = context;
+        prefs = context.getSharedPreferences(MainActivity.PREFERENCES, Context.MODE_PRIVATE);
+        editor = prefs.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return prefs.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+}

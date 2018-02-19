e17ea2f91009cd9b84a1c7fdcbb965b2f0fef304
Patryk Dampc <p.dampc@gmail.com>
2018-02-19 16:31:54 +0100

    splash

--- app/src/main/java/com/example/patryk/divideby3/presenter/SplashActivity.java
+++ app/src/main/java/com/example/patryk/divideby3/presenter/SplashActivity.java
@@ -5,15 +5,26 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.patryk.divideby3.R;
+import com.example.patryk.divideby3.util.PreferenceManager;
+
 /**
  * Created by patryk on 19.02.2018.
  */
 
 public class SplashActivity extends AppCompatActivity {
+    private PreferenceManager prefManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        prefManager = new PreferenceManager(this);
+        if (!prefManager.isFirstTimeLaunch()) {
+            startActivity(new Intent(this, StartActivity.class));
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left);
+            finish();
+        }
         startActivity(new Intent(this, TutorialActivity.class));
         finish();
     }

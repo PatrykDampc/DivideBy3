ed448228c557456b83de6e3c0395b17329e20a4c
Patryk Dampc <p.dampc@gmail.com>
2018-02-19 16:45:37 +0100

    animations

--- app/src/main/java/com/example/patryk/divideby3/presenter/SplashActivity.java
+++ app/src/main/java/com/example/patryk/divideby3/presenter/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
         prefManager = new PreferenceManager(this);
         if (!prefManager.isFirstTimeLaunch()) {
             startActivity(new Intent(this, StartActivity.class));
-            overridePendingTransition(R.anim.slide_from_down,R.anim.slide_to_up);
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left);
             finish();
         }
         startActivity(new Intent(this, TutorialActivity.class));

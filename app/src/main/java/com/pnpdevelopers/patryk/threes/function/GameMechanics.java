12358e7fe5bcaa066fae5a5cc5d1d875b494bb0c
chudzian <pszennyp@gmail.com>
2018-04-04 11:29:39 +0200

    number in gamemechanics

--- app/src/main/java/com/pnpdevelopers/patryk/threes/function/GameMechanics.java
+++ app/src/main/java/com/pnpdevelopers/patryk/threes/function/GameMechanics.java
@@ -2,9 +2,15 @@ package com.pnpdevelopers.patryk.threes.function;
 
 import android.os.Handler;
 
+import com.pnpdevelopers.patryk.threes.model.LevelNumbers;
+
 public abstract class GameMechanics {
     private Handler handler;
     private Runnable runnable;
+    private LevelNumbers levelNumbers;
+    int[] gameArray;
+    int number;
+    private int iterator;
 
     public int getTime() {
         return time;
@@ -19,6 +25,7 @@ public abstract class GameMechanics {
 
     public void startGameTimer() {
         onTimerStart();
+        nextNumber();
         handler = new Handler();
         runnable = () -> {
            onTimerFinish();
@@ -40,5 +47,17 @@ public abstract class GameMechanics {
 
     protected abstract void onTimerFinish();
 
+    public void setUp(){
+        levelNumbers = new LevelNumbers();
+        gameArray = levelNumbers.getGameArray();
+        iterator=0;
+    }
+    private void nextNumber(){
+        this.number = gameArray[iterator];
+        iterator++;
+    }
 
+    public int getNumber() {
+        return number;
+    }
 }
